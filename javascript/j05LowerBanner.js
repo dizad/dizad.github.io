282155f2f465a619d163325e0202df639bc5a565
dizad <74478441+dizad@users.noreply.github.com>
2026-08-07 16:06:51 -0400

    Add files via upload

--- javascript/j05LowerBanner.js
+++ javascript/j05LowerBanner.js
@@ -6,6 +6,6 @@ $('document').ready(function(){
 		'<a href = "https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button" target="_blank" class = "refIcon"><i class = "fa fa-fw fa-linkedin"></i></a>' + 
 		'<a href = "https://github.com/dizad/" target="_blank" class = "refIcon"><i class = "fa fa-fw fa-github"></i></a>' +
 		'<a href = "https://www.freecodecamp.com/dizad87" target="_blank" class = "refIcon"><i class="fa fa-free-code-camp" aria-hidden="true"></i></a>' +
-		'</div><br><span style="filter:brightness(200%); font-size: 12px;">version: <strong>2.19</strong> - last update: <strong>08 / 02 / 26</strong></span>'
+		'</div><br><span style="filter:brightness(200%); font-size: 12px;">version: <strong>2.19</strong> - last update: <strong>08 / 07 / 26</strong></span>'
 	);
 });
